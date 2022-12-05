c06c06fd813271b8b2ff2c3843ebf91c990003de
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-12-05 20:00:29 -0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 import TodoContainer from "./functionBased/components/TodoContainer";
 
@@ -8,7 +8,7 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/react-todo-project">
       <TodoContainer />
     </Router>
   </React.StrictMode>,
